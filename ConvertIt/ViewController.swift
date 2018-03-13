cb4358089feea0494b345dba737a653701b1ca43
JB Computer <bruggemj@bc.edu>
2018-03-13 13:15:25 -0700

    Commit for video 1

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var fromUnitsLabel: UILabel!
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var formulaPicker: UIPickerView!
 
+    
+    var formulasArray = ["miles to kilometers",
+                         "kilometers to miles",
+                         "feet to meters",
+                         "meters to feet",
+                         "yards to meters",
+                         "meters to yards"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        formulaPicker.dataSource = self
+        formulaPicker.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
     }
-
 
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return formulasArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return formulasArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        fromUnitsLabel.text = formulasArray[row]
+    }
+}
